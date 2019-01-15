0c3f9f7ab5a0ba5875b30c8ed3272c7e8f2f3d06
zhouqi <15821471672@163.com>
2019-01-15 21:51:17 +0800

    rqueue 重构位swdmq

---
+++ sword-data-swdmq/src/main/java/com/zq/sword/array/data/swdmq/msg/SwdMsgDeserializer.java
@@ -0,0 +1,47 @@
+package com.zq.sword.array.data.swdmq.msg;
+
+
+import com.zq.sword.array.data.ObjectDeserializer;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @program: sword-array
+ * @description: 默认的数据序列化工具
+ * @author: zhouqi1
+ * @create: 2018-10-17 15:06
+ **/
+public class SwdMsgDeserializer implements ObjectDeserializer<SwdMsg> {
+
+    @Override
+    public SwdMsg deserialize(byte[] data) {
+        SwdMsg swdMsg = new SwdMsg();
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data);
+        Long id = byteBuffer.getLong();
+        swdMsg.setMsgId(id == 0 ? null : id);
+
+        int topicLen = byteBuffer.getInt();
+        if(topicLen > 0){
+            byte[] topicBytes = new byte[topicLen];
+            byteBuffer.get(topicBytes);
+            swdMsg.setTopic(new String(topicBytes));
+        }
+
+        int tagLen = byteBuffer.getInt();
+        if(tagLen > 0){
+            byte[] tagBytes = new byte[tagLen];
+            byteBuffer.get(tagBytes);
+            swdMsg.setTag(new String(tagBytes));
+        }
+
+        int len = byteBuffer.getInt();
+        if(len > 0){
+            byte[] bodyBytes = new byte[len];
+            byteBuffer.get(bodyBytes);
+            swdMsg.setBody(bodyBytes);
+        }
+
+        swdMsg.setTimestamp(byteBuffer.getLong());
+        return swdMsg;
+    }
+}
---
+++ sword-data-swdmq/src/main/java/com/zq/sword/array/data/swdmq/msg/SwdMsgSerializer.java
@@ -0,0 +1,44 @@
+package com.zq.sword.array.data.swdmq.msg;
+
+import com.zq.sword.array.data.ObjectSerializer;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @program: sword-array
+ * @description: 默认的数据序列化工具
+ * @author: zhouqi1
+ * @create: 2018-10-17 15:06
+ **/
+public class SwdMsgSerializer implements ObjectSerializer<SwdMsg> {
+
+    @Override
+    public byte[] serialize(SwdMsg swdMsg) {
+
+        long msgId = swdMsg.getMsgId();
+        String topic = swdMsg.getTopic();
+        String tag = swdMsg.getTag();
+        byte[] body = swdMsg.getBody();
+        long timestamp  = swdMsg.getTimestamp();
+
+
+        int capacity = 8 + 4 + topic.length() + 4 + tag.length() + 4 + body.length + 8;
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(capacity);
+        byteBuffer.putLong(msgId);
+        byteBuffer.putInt(topic.length());
+        if(topic.length() > 0){
+            byteBuffer.put(topic.getBytes());
+        }
+        byteBuffer.putInt(tag.length());
+        if(tag.length() > 0){
+            byteBuffer.put(tag.getBytes());
+        }
+        byteBuffer.putInt(body.length);
+        if(body.length > 0){
+            byteBuffer.put(body);
+        }
+        byteBuffer.putLong(timestamp);
+        return byteBuffer.array();
+    }
+}
