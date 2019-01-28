6b212d2429ecf5083cc55f6293a440a8854bc03f
zhouqi <15821471672@163.com>
2019-01-28 15:42:23 +0800

    完善处理循环命令写入的数据结构

--- sword-redis/src/main/java/com/zq/sword/array/redis/handler/SimpleCycleDisposeHandler.java
+++ sword-redis/src/main/java/com/zq/sword/array/redis/handler/SimpleCycleDisposeHandler.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -23,15 +25,15 @@ public class SimpleCycleDisposeHandler implements CycleDisposeHandler<RedisComma
 
     private Set<RedisCommand> consumedSwordDataSet;
 
+    private DelayQueue<DelayedCommand> commandDelayQueue;
+
     private TimedTaskExecutor taskExecutor;
 
     public SimpleCycleDisposeHandler() {
         logger.info("SwordCommandCycleDisposeBridge 模块启动成功");
         consumedSwordDataSet = new CopyOnWriteArraySet<>();
+        commandDelayQueue = new DelayQueue<>();
         taskExecutor = new SingleTimedTaskExecutor();
-    }
-
-    public void start(){
         startTasks();
     }
 
@@ -42,7 +44,8 @@ public class SimpleCycleDisposeHandler implements CycleDisposeHandler<RedisComma
 
         //只保存两分钟的数据
         taskExecutor.timedExecute(()->{
-            consumedSwordDataSet.clear();
+            DelayedCommand delayedCommand = commandDelayQueue.poll();
+            consumedSwordDataSet.remove(delayedCommand.command);
         }, 2, TimeUnit.MINUTES);
     }
 
@@ -54,6 +57,34 @@ public class SimpleCycleDisposeHandler implements CycleDisposeHandler<RedisComma
 
     @Override
     public boolean addCycleData(RedisCommand command) {
+        commandDelayQueue.add(new DelayedCommand(command, System.currentTimeMillis() + 2000));
         return consumedSwordDataSet.add(command);
+    }
+
+
+    private class DelayedCommand implements Delayed {
+
+        private RedisCommand command;
+
+        private long expires;
+
+        public DelayedCommand(RedisCommand command, long expires) {
+            this.command = command;
+            this.expires = expires;
+        }
+
+        @Override
+        public long getDelay(TimeUnit unit) {
+            return expires - System.currentTimeMillis();
+        }
+
+        @Override
+        public int compareTo(Delayed o) {
+            if(o instanceof DelayedCommand){
+                DelayedCommand delayedCommand = (DelayedCommand)o;
+                return this.equals(delayedCommand) ? 0 : (int)(this.expires - delayedCommand.expires);
+            }
+            return 0;
+        }
     }
 }
