249a1c15f8fae93a1a2789d5d16fdee0fa35f4e5
zhouqi <15821471672@163.com>
2019-06-10 15:28:13 +0800

    admin 添加

---
+++ sword-zadmin/src/test/java/com/zq/sword/array/admin/PiperServerTest.java
@@ -0,0 +1,22 @@
+package com.zq.sword.array.admin;
+
+import org.junit.Test;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @program: sword-array
+ * @description: 服务启动器
+ * @author: zhouqi1
+ * @create: 2018-10-19 10:15
+ **/
+
+public class PiperServerTest {
+
+    @Test
+    public void server1() throws UnsupportedEncodingException {
+        String city = new String("重庆".getBytes(), "utf-8");
+        System.out.println(city);
+    }
+
+}
